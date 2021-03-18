678ade803c1ea898914d4949de8efdc8bf762d29
Nathan Miller <natem31190@gmail.com>
2021-03-18 13:47:49 -0400

    commit tree c08a49fd0007fe18d4683f598fa9bd64acf62978

--- app.rb
+++ app.rb
@@ -2,5 +2,30 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name].reverse
+    "#{@name}"
+    
+end
+
+  get '/square/:number' do
+    @num = params[:number].to_i * params[:number].to_i
+    "#{@num}"
+
+end
+
+# get '/say/:number/:phrase' do
+#     @num = params[:number].to_i
+#     @phrase = params[:phrase].to_s
+#     "show me the money#{num}"
+#     "you had me at hello#{phrase}"
+    get '/say/:number/:phrase' do
+      params[:number].times do 
+       params[:phrase]
+     end
+
+end
+
+# get '/say/:word1/:word2/:word3/:word4/:word5' do
 
 end
